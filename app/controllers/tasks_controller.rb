cc2e8d08c2febc01c56a73ac5d2a37676dc6d178
Albert Diaz <l2claims@yahoo.com>
2020-03-03 15:14:34 -0500

    removed unneeded routes,views and controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:destroy]
 
   # GET /tasks
   # GET /tasks.json
   def index
     @tasks = Task.all
+    @task = Task.new
   end
 
   # GET /tasks/1
@@ -22,43 +23,22 @@ class TasksController < ApplicationController
   end
 
   # POST /tasks
-  # POST /tasks.json
+  
   def create
     @task = Task.new(task_params)
-
-    respond_to do |format|
-      if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
-      else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
+      @task.save
+      redirect_to root_path, notice: 'Task was successfully created.' 
   end
 
   # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
   def destroy
     @task.destroy
-    respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+     redirect_to root_path, notice: 'Task was successfully destroyed.' 
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :tasks, only [:create, :destroy]
+  resources :tasks, only: [:create, :destroy]
   root 'tasks#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
