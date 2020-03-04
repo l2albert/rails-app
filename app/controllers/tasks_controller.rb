ecd869bb451f8430a1819cfb3f68e06f69c3caab
Albert Diaz <l2claims@yahoo.com>
2020-03-04 14:34:47 -0500

    Added bootstrap to better style the form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,27 +8,12 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
-  def show
-  end
-
-  # GET /tasks/new
-  def new
-    @task = Task.new
-  end
-
-  # POST /tasks
-  
   def create
     @task = Task.new(task_params)
       @task.save
       redirect_to root_path, notice: 'Task was successfully created.' 
   end
 
-  # PATCH/PUT /tasks/1
-  def update
-  end
 
   # DELETE /tasks/1
   # DELETE /tasks/1.json
